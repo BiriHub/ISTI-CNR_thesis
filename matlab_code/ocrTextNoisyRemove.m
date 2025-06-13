7040bd0fcdc678fad50ffeb5dc6e709c73a5c569
Biri <leonardo09012003@gmail.com>
2025-06-13 10:49:28 +0200

    Added logarithmic value for not-labeled lines

--- matlab_code/ocrTextNoisyRemove.m
+++ matlab_code/ocrTextNoisyRemove.m
@@ -1,4 +1,4 @@
-function cleaned_ocrTextResults = ocrTextNoisyRemove(ocrTextResults)
+function [cleaned_ocrTextResults, idx] = ocrTextNoisyRemove(ocrTextResults)
     cleaned_ocrTextResults = ocrTextResults(:);
 
     % Preallocate numeric array
@@ -27,17 +27,33 @@ function cleaned_ocrTextResults = ocrTextNoisyRemove(ocrTextResults)
     end
 
     % Filter out non‐increasing values
+    
     keep_idx = true(n, 1);       % Start by keeping all
     current_value = numeric_values(1);  % Initialize with the first value
 
+    idx=zeros(n,1);
+    idx(1)= current_value;
     for i = 2:n
         if numeric_values(i) > current_value
-            current_value = numeric_values(i);  % Update reference value
+            % Update both current and previous values
+            current_value = numeric_values(i);
         else
+            % Calculate logarithmic midpoint
+            log_prev = log10(current_value);
+            log_current = log10(numeric_values(i+1));
+            log_mid = (log_prev + log_current) / 2;
+            
+            % Set new value to 10^(logarithmic midpoint)
+            new_value = 10^log_mid;
+            
+            % Update the current value to the new midpoint value
+            numeric_values(i) = new_value;
+            current_value = new_value;
             keep_idx(i) = false;  % Mark for removal
         end
+        idx(i)=current_value;
     end
-
+    
     % Apply filtered results
     cleaned_ocrTextResults = cleaned_ocrTextResults(keep_idx);
 
